ab3bb358aacaf9a49ffc8e4b0f1e18968bc12372
Daniel Noguchi <daniel.noguchi@sv.cmu.edu>
2014-10-12 13:42:18 -0700

    Add rest api mappings for private chat messages

--- config/rest_api.js
+++ config/rest_api.js
@@ -1,10 +1,14 @@
 var host_url = "http://localhost:1234/ssnoc";
 
 module.exports = {
-  'get_all_users' : host_url + '/users',
-  'is_password_valid' : host_url + '/user/',
-  'get_user' : host_url + '/user/',
-  'post_new_user' : host_url + '/user/signup',
-  'post_new_status' : host_url + '/status/new',
-  'get_all_statuses' : host_url + '/statuses'
+  'get_all_users'              : host_url + '/users',
+  'is_password_valid'          : host_url + '/user/',
+  'get_user'                   : host_url + '/user/',
+  'post_new_user'              : host_url + '/user/signup',
+  'post_new_status'            : host_url + '/status/new',
+  'get_all_statuses'           : host_url + '/statuses',
+  'send_message'               : host_url + '/message/',
+  'get_all_msgs_between_users' : host_url + '/message/',
+  'get_chats'                  : host_url + '/message/',
+  'get_message_by_id'          : host_url + '/message/'
 };
