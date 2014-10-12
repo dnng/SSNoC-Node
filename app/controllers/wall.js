5a4f3254f4e81804e89dc0ca48cd10dce77fd5a9
Daniel Noguchi <daniel.noguchi@sv.cmu.edu>
2014-10-12 11:44:52 -0700

    Update code style

--- app/controllers/wall.js
+++ app/controllers/wall.js
@@ -1,6 +1,6 @@
 var LocalStrategy = require('passport-local').Strategy;
-var request = require('request');
-var Status = require('../models/StatusRest');
+var request       = require('request');
+var Status        = require('../models/StatusRest');
 
 module.exports = function(_, io, participants, passport) {
   return {
