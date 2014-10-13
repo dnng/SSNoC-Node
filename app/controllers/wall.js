344121203fed6855171c44fa4a88430d0ba097ca
anson1991 <anson.ling@west.cmu.edu>
2014-10-13 14:35:28 -0700

    Add Wall update method

--- app/controllers/wall.js
+++ app/controllers/wall.js
@@ -1,17 +1,26 @@
 var LocalStrategy = require('passport-local').Strategy;
 var request = require('request');
-var Status = require('../models/StatusRest');
+var WallMessage = require('../models/WallRest');
 
 module.exports = function(_, io, participants, passport) {
   return {
     getStatuses: function(req, res) {
-      Status.getAllStatuses(function(err, statuses) {
+        WallMessage.getAllStatuses(function(err, statuses) {
         console.log(statuses);
         if (err)
           return res.redirect('/welcome');
         res.render("wall", {userId: req.session.userId, title:"Statuses", user_name:req.session.passport.user.user_name, statuses: statuses});
       });
-    }
+    },
+
+    postWallUpdate: function(req, res, next) {
+        WallMessage.saveNewWallMessage(req.session.passport.user.user_name, req.body.message, req.body.status, req.body.location, function(err, new_user) {
+          if (err)
+            console.log(err);
+            return res.redirect('/welcome');
+          return res.redirect('/wall');
+        });
+        }
   };
 };
 
