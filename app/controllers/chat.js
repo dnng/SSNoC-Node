c5ce173ed49156ced21cbc2dfba61f552b77550b
Daniel Noguchi <daniel.noguchi@sv.cmu.edu>
2014-10-13 15:43:23 -0700

    Change space for tab

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -8,7 +8,7 @@ module.exports = function(_, io, participants, passport) {
       var user_name1 = req.session.passport.user.user_name;
       var user_name2 = req.session.passport.user.user_name;
       Chat.getAllChatMessagesBetweenUsers(user_name1, user_name2, function (err, chats) {
-    	console.log(chats);
+      console.log(chats);
         if (chats !== null) {
           res.json(200, {name: user.local.name});
         }
