8cfb5556e78f4322125b80e4f5ae1b4ab9ab3850
Daniel Noguchi <daniel.noguchi@sv.cmu.edu>
2014-10-14 12:45:19 -0700

    controller.chat: Add initial parsing of users names

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,13 +1,20 @@
 var LocalStrategy = require('passport-local').Strategy;
 var request       = require('request');
 var Chat          = require('../models/ChatRest');
+var url           = require('url');
 
 module.exports = function(_, io, participants, passport) {
   return {
     getAllMessages: function (req, res) {
-      var user_name1 = req.session.passport.user.user_name;
-      var user_name2 = req.session.passport.user.user_name;
-      Chat.getAllChatMessagesBetweenUsers(user_name1, user_name2, function (err, chats) {
+      var url_parts  = url.parse(req.url, true);
+      var query      = url_parts.query;
+      var author_name = query.author_name;
+      var target_name = query.target_name;
+      console.log("app.controllers.chat: DEBUG :");
+      console.log(query);
+      console.log(author_name);
+      console.log(target_name);
+      Chat.getAllChatMessagesBetweenUsers(author_name, target_name, function (err, chats) {
       console.log(chats);
         if (chats !== null) {
           res.json(200, {name: user.local.name});
