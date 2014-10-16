dc8d58c3f64959e2e36453db9553eef92a8b57a3
anson1991 <anson.ling@west.cmu.edu>
2014-10-15 18:37:25 -0700

    Little change about the body of Clusters

--- app/models/AnalysisRest.js
+++ app/models/AnalysisRest.js
@@ -2,33 +2,29 @@ var bcrypt = require('bcrypt-nodejs');
 var request = require('request');
 var rest_api = require('../../config/rest_api');
 
-function Clusters(clusteruser_id){
+function Clusters(timeWindow, userClusters){
 	  this.local = {
-        clusteruser_id : clusteruser_id
+        timeWindow : timeWindow,
+	    userClusters : userClusters
 	  };
 	}
 
 Clusters.getUserClusters = function(timeWindow,callback) {
-	  request(rest_api.get_user_clusters(timeWindow), {json:true}, function(err, res, body) {
-		rest_api.get_user_clusters = function(timeWindow){
-			return host_url + '/usergroups/unconnected/' + timeWindow;
-			}
-		  if (err){
-	      callback(err,null);
-	      return;
-	    }
+	  request(rest_api.get_user_clusters, {json:true, body:{timeWindow:timeWindow}}, function(err, res, body) {
+        if (err){
+          callback(err,null);
+          return;
+        }
 	    if (res.statusCode === 200) {
-	      var userClusters = body.map(function(item, idx, arr){
-	        return new Clusters(item.clusteruserId);
-	      });
+		  var clusters = new Clusters(body.userClusters, body.timeWindow);
+		  callback(null, clusters);
+		  return;
+	    }
+		if (res.statusCode !== 200) {
+		  callback(null, null);
+		  return;
+	        	}
+	          });
+	        };
 
-	      console.log("@@@@@ in Status.getUserClusters succeed statuses :" + JSON.stringify(wallMessages));
-	      callback(null, userClusters);
-	      return;
-	    }
-	    if (res.statusCode !== 200) {
-	      callback(null, null);
-	      return;
-	    }
-	  });
-	};
+
