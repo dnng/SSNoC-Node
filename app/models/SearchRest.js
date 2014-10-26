0a041529d61df9f5edab010b379c94bd5bf5544f
nehaa <neha.arora.cs@gmail.com>
2014-10-25 19:43:55 -0700

    data placeholder for search operations

---
+++ app/models/SearchRest.js
@@ -0,0 +1,21 @@
+var bcrypt = require('bcrypt-nodejs');
+var request = require('request');
+var rest_api = require('../../config/rest_api');
+
+function Search(context, search_string, user_name, status, anouncement, public_message, private_message, sender_name, timestamp, location){
+	  this.local = {
+	    context : context, 
+	    search_string : search_string,
+	    user_name : user_name,
+	    status : status,
+	    announcement : announcement,
+	    public_message : public_message,
+	    private_message : private_message,
+	    sender_name : sender_name,
+	    timestamp : timestamp,
+	    location : location  
+	  };
+	}
+
+
+module.exports = Search;
