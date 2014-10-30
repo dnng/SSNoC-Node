61ee0a137bfab4f65cfae22749188f4912ff4918
nehaa <neha.arora.cs@gmail.com>
2014-10-30 13:29:03 -0700

    stop words in progress

--- app/models/SearchRest.js
+++ app/models/SearchRest.js
@@ -21,12 +21,51 @@ function Search(context, search_string, user_name, status, anouncement, public_m
 
 Search.remove_stop_words = function(stop_words_url, search_string) {
 	var stop_words = request(stop_words_url);
-	//var stop_word_array = stop_words.split(",");
-	//console.log("Stop words fetched: " + stop_word_array);
+	var stop_word_array = stop_words.split(",");
+	console.log("Stop words fetched: " + stop_word_array);
 }
 
-Search.getAllUsers = function(user_names, callback) {
+function status_to_process(token, flag){
+	this.local = {
+			token : token,
+			enabled : flag
+	};
+}
+
+Search.getAllUsers = function(search_tokens, callback) {
+	console.log("Processing statuses:\n");
+	// remove status strings from search tokens and store in  
+	var user_names = [];
+	
+	//To-DO use a map instead
+	var ok_status = false;
+	var help_status = false;
+	var emergency_status = false;
+	
+	var status_tokens = new status_to_process(token, flag);
+	search_tokens.forEach(function(token, index, array) {
+		switch (token) {
+			case 'OK':				
+			case 'Help':				
+			case 'Emergency':
+				// ??
+				status_tokens.put(token, true);
+				break;
+			default:
+				user_names.push(token);		
+		}
+	});
+	
+	// process the statuses
+	Object.keys(status_tokens).forEach(function(key) {
+		  var val = status_tokens[key];
+		  console.log ("Statuses to process: " + val);
+		});
+	
+	
+	
 	console.log("Processing user names:\n");
+
 	if(typeof user_names != 'undefined') {
 
 		var search_operation = [];
