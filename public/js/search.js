120b4d05c1c1aafe6927e74d668e28932a188b1f
Poorva Jain <poorva.jain@sv.cmu.edu>
2014-11-11 16:20:50 -0800

    Change for adding pagination for message

--- public/js/search.js
+++ public/js/search.js
@@ -2,6 +2,12 @@ function init() {
 	$('#tbl_announcements').DataTable({
 		  "searching": false
 	});
+	$('#tbl_private_message').DataTable({
+		  "searching": false
+	});
+	$('#tbl_public_message').DataTable({
+		  "searching": false
+	});
 }
 
 $(document).on('ready', init);
