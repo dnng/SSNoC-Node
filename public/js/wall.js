bdbf2a9601134b51644ce2566bb98661972e5f52
Lloyd D'Silva <lloyddsilva@gmail.com>
2014-11-20 05:15:30 -0800

    Added modernizr and basic geolocation

--- public/js/wall.js
+++ public/js/wall.js
@@ -1,117 +1,46 @@
 function init() {
-  var serverBaseUrl = document.domain;
+    var serverBaseUrl = document.domain;
 
-  var socket = io.connect(serverBaseUrl);
+    var socket = io.connect(serverBaseUrl);
 
-  var sessionId = '';
+    var sessionId = '';
 
-  window.my_name = '';
-/*
-  function updateParticipants(participants) {
-    $('#participants_online').html('');
-    $('#participants_offline').html('');
-    var map = {};
-    var userName = '';
-    var userEle = '';
-    for (var sId in participants.online){
-      userName = participants.online[sId].userName;
-      if (map[userName] == undefined || map[userName] !== sessionId){
-        map[userName] = {sId:sId};
-      }
-    }
-    keys = Object.keys(map);
-    keys.sort();
+    window.my_name = '';
 
-    for (var i = 0; i < keys.length; i++) {
-      var name = keys[i];
-      var img_ele = '<img src="/img/photo4.png" height=40/>';
-      var photo_ele = '<div class="col-xs-3 col-sm-2 col-md-1 col-lg-1"><img src="/img/green-dot.png" height=10/><br/>'+img_ele + '</div>';
-      var name_ele = '<div class="col-xs-8 col-sm-9 col-md-10 col-lg-10"><strong>' + name + '</strong></div>';
-      var dropdown_symbol = map[name].sId === sessionId ? '':'<i class="glyphicon glyphicon-chevron-down text-muted"></i>';
-      var dropdown_ele = '<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 dropdown-user" data-for=".' + name + '">' + dropdown_symbol + '</div>';
+    socket.on('error', function(reason) {
+        console.log('Unable to connect to server', reason);
+    });
 
-      var info_ele = '<div class="row user-row search_item">' + photo_ele + name_ele + dropdown_ele + '</div>';
-      var detail_ele = '<div class="row user-info ' + name + '"><a class="btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xs-offset-3 col-sm-offset-3 col-md-offset-3 col-lg-offset-3">Wanna do something?</a><hr/></div></div>';
-      if (map[name].sId === sessionId || name === my_name) {
-      } else {
-        $('#participants_online').append(info_ele);
-        $('#participants_online').append(detail_ele);
-      }
+    // check whether browser supports geolocation api
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(positionSuccess, positionError, {
+            enableHighAccuracy: true
+        });
+    } else {
+        console.log("Your browser is out of fashion, there\'s no geolocation!");
     }
 
-    participants.all.forEach(function(userObj) {
-      if (map[userObj.userName] == undefined) {
-        var img_ele = '<img class="img-circle" src="/img/photo4.png" height=40/>';
-        var photo_ele = '<div class="offline col-xs-3 col-sm-2 col-md-1 col-lg-1"><img src="/img/grey-dot.png" height=10/><br/>'+img_ele + '</div>';
-        var name_ele = '<div class="offline col-xs-8 col-sm-9 col-md-10 col-lg-10"><strong>' + userObj.userName + '</strong><br/></div>';
-        var dropdown_ele = '<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 dropdown-user" data-for=".' + userObj.userName + '"><i class="glyphicon glyphicon-chevron-down text-muted"></i></div>';
-        var info_ele = '<div class="row user-row search_item">' + photo_ele + name_ele + dropdown_ele + '</div>';
-        var detail_ele = '<div class="row user-info ' + userObj.userName + '"><a class="btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xs-offset-3 col-sm-offset-3 col-md-offset-3 col-lg-offset-3">Wanna do something?</a><hr/></div></div>';
-        $('#participants_online').append(info_ele);
-        $('#participants_online').append(detail_ele);
-      }
-    });
-    $('.user-info').hide();
-    $('.dropdown-user').click(function() {
-      var dataFor = $(this).attr('data-for');
-      var idFor = $(dataFor);
-      var currentButton = $(this);
-      idFor.slideToggle(400, function() {
-        if(idFor.is(':visible'))
-          {
-            currentButton.html('<i class="glyphicon glyphicon-chevron-up text-muted"></i>');
-          }
-          else
-            {
-              currentButton.html('<i class="glyphicon glyphicon-chevron-down"></i>');
-            }
-      })
-    });
-  }*/
+    function positionSuccess(position) {
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+        var acr = position.coords.accuracy;
+        console.log("latitude:" + lat + ", longitude:" + lng + ", accuracy:" + acr);
+    }
 
-  socket.on('connect', function () {
-    sessionId = socket.socket.sessionid;
-    $.ajax({
-      url:  '/user',
-      type: 'GET',
-      dataType: 'json'
-    }).done(function(data) {
-      var name = data.name;
-      my_name = data.name;
+    // handle geolocation api errors
+    function positionError(error) {
+        var errors = {
+            1: "Authorization fails", // permission denied
+            2: "Can\'t detect your location", //position unavailable
+            3: "Connection timeout" // timeout
+        };
+        showError("Error:" + errors[error.code]);
+    }
 
-      socket.emit('newUser', {id: sessionId, name: name});
-    });
-  });
-/*
-  socket.on('newConnection', function (data) {
-    updateParticipants(data.participants);
-  });
+    function showError(msg) {
+        console.log(msg);
+    }
 
-  socket.on('userDisconnected', function(data) {
-    updateParticipants(data.participants);
-  });*/
-
-  socket.on('error', function (reason) {
-    console.log('Unable to connect to server', reason);
-  });
-
-  var panels = $('.user-info');
-  panels.hide();
-  $('.dropdown-user').click(function() {
-    var dataFor = $(this).attr('data-for');
-    var idFor = $(dataFor);
-    var currentButton = $(this);
-    idFor.slideToggle(400, function() {
-      if(idFor.is(':visible'))
-        {
-          currentButton.html('<i class="glyphicon glyphicon-chevron-up text-muted"></i>');
-        }
-        else
-          {
-            currentButton.html('<i class="glyphicon glyphicon-chevron-down text-muted"></i>');
-          }
-    })
-  });
 }
 
 $(document).on('ready', init);
