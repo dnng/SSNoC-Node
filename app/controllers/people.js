4b80455fe99924d06a7c2abd6cffd1a3b3e57694
nehaa <neha.arora.cs@gmail.com>
2014-11-28 00:38:40 -0800

    Refactored user search

--- app/controllers/people.js
+++ app/controllers/people.js
@@ -7,7 +7,7 @@ module.exports = function(_, io, participants, passport) {
         User.getUser(user_name, function(err, user) {
           if (user !== null) {
         	  console.log(res);
-        	  res.render("people", { messages: req.flash('info'), userId: req.session.userId, title:"Directory", user_name:req.session.passport.user.user_name, lastStatus:user.local.lastStatus});        	  
+        	  res.render("people", { messages: req.flash('search_alert'), userId: req.session.userId, title:"Directory", user_name:req.session.passport.user.user_name, lastStatus:user.local.lastStatus});        	  
           }
         });
      }
--- app/models/SearchRest.js
+++ app/models/SearchRest.js
@@ -64,67 +64,51 @@ Search.strip_stop_words = function(search_tokens, callback) {
 }
 
 Search.getAllUsers = function(search_tokens, callback) {
-	console.log("Processing statuses:\n");
-	// remove status strings from search tokens and store in  
-	var user_names = [];
-	/*	
-	 //To-DO use a map instead
-	 var ok_status = false;
-	 var help_status = false;
-	 var emergency_status = false;
-	
-	 var status_tokens = new status_to_process(token, flag);
-	 search_tokens.forEach(function(token, index, array) {
-	 switch (token) {
-	 case 'OK':				
-	 case 'Help':				
-	 case 'Emergency':
-	 // ??
-	 status_tokens.put(token, true);
-	 break;
-	 default:
-	 user_names.push(token);		
-	 }
-	 });
-	
-	 // process the statuses
-	 Object.keys(status_tokens).forEach(function(key) {
-	 var val = status_tokens[key];
-	 console.log ("Statuses to process: " + val);
-	 });
-	 */
+	var user_names = {};
+	var status_tokens = {};
+	search_tokens.forEach(function(element, index, array) {
+		var token = element.toUpperCase();
+		switch (token) {		
+		case 'OK':				
+		case 'HELP':				
+		case 'EMERGENCY':
+			status_tokens[token] = true;
+			break;
+		default:
+			user_names[element] = true;		
+		}
+	});
 
-	//
-	user_names = search_tokens;
-	console.log("Processing user names:\n");
-
-	if (typeof user_names != 'undefined') {
-
-		var search_operation = [];
-		user_names.forEach(function(val, index, array) {
-			search_operation.push(function(callback) {
-				User.getUser(val,
-						function(err, user) {
-							if (user !== null) {
-								console.log("fetched user details: "
-										+ user.local.name);
+	var result = {};
+	if (typeof user_names !== 'undefined' || typeof status_tokens !== 'undefined') {
+		User.getAllUsers(function(err, users) {
+			if (users !== null) {
+				for(var name in user_names) {						
+					users.forEach(function(val, index, array) {
+						if (name.toUpperCase().indexOf(val.local.name.toUpperCase()) != -1) {
+							if (typeof result[name] == 'undefined') {
+								result[name] = [];
 							}
-							callback(null, user);
-						});
-			});
-		});
-
-		async.parallel(search_operation, function(error, results) {
-			var users = [];
-			for (var i = 0; i < results.length; i++) {
-				if (results[i]) {
-					users.push(results[i]);
+							result[name].push(val);
+						}	
+						console.log (name + " : " + result[name]);
+					});
+				}
+				
+				for(var status in status_tokens) {	
+					users.forEach(function(val, index, array) {
+						if (val.local.lastStatus && status.toUpperCase().indexOf(val.local.lastStatus.toUpperCase()) != -1) {
+							if (typeof result[status] == 'undefined') {
+								result[status] = [];
+							}
+							result[status].push(val);
+						}	
+					});
 				}
 			}
+			//To-Do: return and render results
 			callback(null, users);
 		});
-	} else {
-		console.log("none found");
 	}
 }
 
